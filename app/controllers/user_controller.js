b51ad9858c7a3ca12cab335d70f22b8e7e598ac6
lidingkorol <799834285@qq.com>
2017-06-05 18:21:34 +0800

    t

---
+++ app/controllers/user_controller.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Administrator on 2017/6/3.
+ */
+
+
+//获取用户
+exports.getUser = async (ctx, next) => {
+    ctx.body = {
+        username: '阿，希爸',
+        age: 30
+    }
+}
+
+//用户注册
+exports.registerUser = async (ctx, next) => {
+    console.log('registerUser', ctx.request.body);
+}
---
+++ init/index.js
@@ -0,0 +1,27 @@
+
+
+const mysql = require('mysql')
+
+// 创建数据池
+const pool  = mysql.createPool({
+  	host     : '127.0.0.1',   // 数据库地址
+  	user     : 'root',    // 数据库用户
+  	password : '123456'   // 数据库密码
+  	database : 'serkoa2'  // 选中数据库
+})
+ 
+// 在数据池中进行会话操作
+pool.getConnection(function(err, connection) {
+   
+  	connection.query('SELECT * FROM my_table',  (error, results, fields) => {
+    
+    	// 结束会话
+    	connection.release();
+ 
+    	// 如果有错误就抛出
+    	if (error) throw error;
+  	})
+})
+
+
+
