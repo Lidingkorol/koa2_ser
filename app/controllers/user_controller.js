f43b1daad26f4c1d96da54dd507a87497753f1ac
lidingkorol <799834285@qq.com>
2017-06-06 18:24:05 +0800

    t

--- app/controllers/user_controller.js
+++ app/controllers/user_controller.js
@@ -2,11 +2,28 @@
  * Created by Administrator on 2017/6/3.
  */
 
-
+var mysql = require('mysql');
+const { query } = require('../../init/index')
 //获取用户
 exports.getUser = async (ctx, next) => {
+	var connection = mysql.createConnection({
+	    host: 'localhost',
+	    user: 'root',
+	    password: 'admin',
+	    database:'mysql'
+	});
+	
+	connection.connect();
+	//查询
+	connection.query('select * from `test` where name = 1 ', function(err, rows, fields) {
+	    if (err) throw err;
+	    console.log(rows)
+	});
+	//关闭连接
+	connection.end();
     ctx.body = {
-        username: '阿，希爸',
+    	
+        username: 'asdasd',
         age: 30
     }
 }
--- init/index.js
+++ init/index.js
@@ -6,22 +6,88 @@ const mysql = require('mysql')
 const pool  = mysql.createPool({
   	host     : '127.0.0.1',   // 数据库地址
   	user     : 'root',    // 数据库用户
-  	password : '123456'   // 数据库密码
-  	database : 'serkoa2'  // 选中数据库
+  	password : 'admin',   // 数据库密码
+  	database : 'mysql'  // 选中数据库
 })
  
-// 在数据池中进行会话操作
-pool.getConnection(function(err, connection) {
-   
-  	connection.query('SELECT * FROM my_table',  (error, results, fields) => {
-    
-    	// 结束会话
-    	connection.release();
- 
-    	// 如果有错误就抛出
-    	if (error) throw error;
+/*let query = function( sql, values ) {
+  	return new Promise(( resolve, reject ) => {
+	    pool.getConnection(function(err, connection) {
+	      	if (err) {
+	        	reject( err )
+	      	} 
+	      	else {
+	    		connection.query(sql, values, ( err, rows) => {
+		
+		          	if ( err ) {
+		            	reject( err )
+		          	} else {
+		            	resolve( rows )
+		          	}
+		          	connection.release()
+		        })	
+	      	}
+	    })
   	})
-})
+}*/
+//数据库基本操作方法
+const query = function ( sql, values, callback ) {
+    pool.getConnection(function(err, conn) {
+        if (err) {
+            callback(err, false)
+        } else {
+            var querys = conn.query(sql, values, function(err, rows) {
+
+                if (err) {
+                    callback(err, false)
+                } else {
+                    callback(null,rows)
+                }
+            }); 
+        }
+    });
+}
+
+//数据库对外操作类
+exports.DatabaseUtil = {
+
+    //建表方法
+    createTable ( sql, callback ) {
+        query( sql, [], callback )
+    },
+
+    //根据id查找数据
+    findDataById ( table,  id, callback) {
+        let  _sql =  "select * from ?? where id = ? "
+        query( _sql, [ table, id], callback )
+    },
+
+    //分页查找数据
+    findDataByPage ( table, start, end , callback) {
+        let  _sql =  "select * from ??  limit ? , ?"
+        query( _sql, [ table,  start, end ], callback )
+    },
+
+    //插入数据
+    insertData ( table, values, callback ) {
+        let _sql = "insert into ?? set ?"
+        query( _sql, [ table, values ], callback )
+    },
+
+    //更新数据
+    updateData ( table, values, id,  callback ) {
+        let _sql = "update ?? set ? where id = ?"
+        query( _sql, [ table, values, id ], callback )
+    },
+
+    //删除数据
+    deleteDataById ( table, id, callback ) {
+        let _sql = "delete from ?? where id = ?"
+        query( _sql, [ table, id ], callback )
+    }
+}
 
 
 
+
+/*module.exports = { query }*/
